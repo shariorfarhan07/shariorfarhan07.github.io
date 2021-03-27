0dd1fee7b8248f5c69fd8cb011c8dfb64d6e0efe
Md Sharior Hossain Farhan <57462157+shariorHossainfarhan@users.noreply.github.com>
2021-03-27 19:50:22 +0600

    added new files

---
+++ app.js
@@ -0,0 +1,38 @@
+const text=document.querySelector(".container");
+const form =document.querySelector('#form');
+
+function renderText(doc){
+let div=document.createElement('div');
+div.classList.add("fromstyle");
+div.setAttribute('data-id',doc.id);
+let p =document.createElement('p')
+p.textContent=doc.data().text;
+div.appendChild(p);
+text.appendChild(div);
+
+}
+
+
+
+
+db.collection('textme').get().then((snapshot)=>{
+ snapshot.docs.forEach(doc => {
+    renderText(doc);
+ });
+
+});
+
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+   if(form.name.value!==''){  
+        console.log(form.name.value!='');
+        db.collection('textme').add({text:form.name.value});
+        form.name.value='';
+       // window.setTimeout(location.reload(),1000);
+        
+
+
+    }
+   
+})
